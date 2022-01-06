8abf9966bd9df39741ff3a0ad32d4de99846072a
Daniel Fenner <dcfenner@gmail.com>
2022-01-06 17:22:14 -0500

    UI progress

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -18,5 +18,12 @@ Book.prototype.info = function () {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-
+    let new_book = new Book(title, author, pages, read);
+    myLibrary.push(new_book);
+    console.log(`${new_book} successfully added to myLibrary array`);
 }
+
+addBookToLibrary("Vampires", "Anne Rice", 365, false);
+addBookToLibrary("Spiderman", "Marvel Guy", 362, true);
+addBookToLibrary("Barney The Dinosaur", "Who Knows", 15, true);
+
