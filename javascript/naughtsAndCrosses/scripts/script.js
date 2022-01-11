d78897f16f8efd51428b58ea2b8b223fc4621a94
Daniel Fenner <dcfenner@gmail.com>
2022-01-11 06:07:33 -0500

    added a scene switcher

--- javascript/naughtsAndCrosses/scripts/script.js
+++ javascript/naughtsAndCrosses/scripts/script.js
@@ -1,20 +1,56 @@
+// Factory function contains all player related attributes and functions
 const Player = (name, symbol) => {
     return { name, symbol };
 };
 
+// Module controls everything related to visuals
 const DisplayController = (() => {
+
+    const sceneSwitch = (scene) => {
+        const menuScene = document.querySelector(".menuScene");
+        const difficultyScene = document.querySelector(".difficultyScene");
+        const gameScene = document.querySelector(".gameScene");
+
+        switch (scene) {
+            case "menu":
+                menuScene.style.display = "flex";
+                difficultyScene.style.display = "none";
+                gameScene.style.display = "none";
+                break;
+            case "difficulty":
+                menuScene.style.display = "none";
+                difficultyScene.style.display = "flex";
+                gameScene.style.display = "none";
+                break;
+            case "game":
+                menuScene.style.display = "none";
+                difficultyScene.style.display = "none";
+                gameScene.style.display = "flex";
+                break;
+        }
+    };
+
+    const playerToggle = () => {
+        const gameInfo = document.querySelector(".gameInfo");
+
+    }
+
+    return { sceneSwitch };
 
 })();
 
+// Game module contains all game related logic
 const Game = (() => {
 
 })();
 
+let cpu = document.querySelector(".cpu");
+let pvp = document.querySelector(".pvp");
 
-let scene1 = document.querySelector(".menuScene");
-let scene2 = document.querySelector(".theGame");
+pvp.addEventListener("click", () => {
+    DisplayController.sceneSwitch("game");
+});
 
-scene1.addEventListener("click", () => {
-    scene1.style.display = "none";
-    scene2.style.display = "flex";
+cpu.addEventListener("click", () => {
+    DisplayController.sceneSwitch("difficulty");
 })
