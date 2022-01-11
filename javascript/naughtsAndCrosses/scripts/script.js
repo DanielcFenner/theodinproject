fe9bb52473453cb3277948cb7014bff4a992a3d3
Daniel Fenner <dcfenner@gmail.com>
2022-01-11 06:43:02 -0500

    added createboard function

--- javascript/naughtsAndCrosses/scripts/script.js
+++ javascript/naughtsAndCrosses/scripts/script.js
@@ -3,6 +3,10 @@ const Player = (name, symbol) => {
     return { name, symbol };
 };
 
+// TEST PLAYERS FOR DEBUGGING PURPOSES
+let players = [Player("Player 1", "X"), Player("Player 2", "O")];
+console.log(players);
+
 // Module controls everything related to visuals
 const DisplayController = (() => {
 
@@ -30,22 +34,43 @@ const DisplayController = (() => {
         }
     };
 
-    const playerToggle = () => {
-        const gameInfo = document.querySelector(".gameInfo");
+    const playerRender = () => {
+        const gameInfoPlayer = document.querySelector(".gameInfoPlayer");
+        gameInfoPlayer.textContent = Game.currentPlayer.name + " ";
+    };
 
-    }
-
-    return { sceneSwitch };
+    return { sceneSwitch, playerRender };
 
 })();
 
 // Game module contains all game related logic
 const Game = (() => {
+    let currentPlayer = players[1];
+
+    // Returns 3x3 array with empty strings
+    const createBoard = () => {
+        let arr = [];
+        let insideArray = [];
+        for (let i = 0; i < 3; i++) {
+            for (let i = 0; i < 3; i++) {
+                insideArray.push("");
+            }
+            arr.push(insideArray);
+            insideArray = [];
+        }
+        return arr;
+    };
+
+
+    return { currentPlayer, createBoard };
 
 })();
 
+console.log(Game.createBoard());
+
 let cpu = document.querySelector(".cpu");
 let pvp = document.querySelector(".pvp");
+
 
 pvp.addEventListener("click", () => {
     DisplayController.sceneSwitch("game");
