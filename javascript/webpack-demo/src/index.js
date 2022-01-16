d1dcd953bac2ba1f8aa35e569a40499adb0e27b4
Daniel Fenner <dcfenner@gmail.com>
2022-01-16 08:17:58 -0500

    Learning webpack

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
-import myName from './myName';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-
+    const btn = document.createElement('button');
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = myName("Dan");
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
 
+    btn.onclick = printMe;
+
+
+    element.appendChild(btn);
     return element;
 }
 
