dcaaedab81fe5457996d95ea7d73564822a424a9
Daniel Fenner <dcfenner@gmail.com>
2022-01-20 09:51:59 -0500

    Todo render function added

--- javascript/todo/src/dom.mjs
+++ javascript/todo/src/dom.mjs
@@ -7,6 +7,12 @@ export default class Dom {
   }
 
   static renderTodo(todo) {
-    const todoContainer = document.querySelector(".todo-Container");
+    const todoContainer = document.querySelector(".todo-container");
+    const div = document.createElement("div");
+    div.classList.add("todo-element");
+    const p = document.createElement("p");
+    p.textContent = todo.title;
+    div.appendChild(p);
+    todoContainer.appendChild(div);
   }
 }
--- javascript/todo/src/index.js
+++ javascript/todo/src/index.js
@@ -7,9 +7,16 @@ let groceries = new List();
 groceries.add(new Todo("Broccoli", "2021", "true"));
 groceries.add(new Todo("Tomatoes", "2022", "false"));
 
+let todoTest1 = new Todo("Broccoli", "2021", "true");
+
 console.log(groceries.listArray);
 
 const addTodo = document.querySelector(".add-todo");
 addTodo.addEventListener("click", () => {
   Dom.resetTodoInput();
 });
+
+Dom.renderTodo(todoTest1);
+Dom.renderTodo(todoTest1);
+Dom.renderTodo(todoTest1);
+Dom.renderTodo(todoTest1);
